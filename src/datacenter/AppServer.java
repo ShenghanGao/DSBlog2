44425a0096d33c982f2157db27ad99e1c5307dec
Shenghan Gao <victorgaohk@gmail.com>
2016-06-03 20:54:05 -0700

    t

--- src/datacenter/AppServer.java
+++ src/datacenter/AppServer.java
@@ -129,11 +129,6 @@ public class AppServer {
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			log = (List<LogEntry>) ois.readObject();
 			ois.close();
-
-			for (LogEntry e : log) {
-				System.out.println(e.getCommand());
-			}
-			System.out.println("\n");
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -681,7 +676,8 @@ public class AppServer {
 					}
 					case APPEND_ENTRIES: {
 						Message response = recvAppendEntries(message);
-						sendMessage(appServer.nodes.get(appServer.currentLeader), response);
+						if (appServer.currentLeader != -1)
+							sendMessage(appServer.nodes.get(appServer.currentLeader), response);
 						break;
 					}
 					case APPEND_ENTRIES_RESPONSE: {
