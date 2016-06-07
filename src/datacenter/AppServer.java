38922ec91bb597c4f88565206e30b34b14ba1505
Shenghan Gao <victorgaohk@gmail.com>
2016-06-07 03:03:56 -0700

    Fix

--- src/datacenter/AppServer.java
+++ src/datacenter/AppServer.java
@@ -218,7 +218,7 @@ public class AppServer {
 		if (!toRecover) {
 			writeCurrentTermFile(1);
 			writeVotedForFile(-1);
-			writeLogFile(new ArrayList<>());
+			writeLogFile(new ArrayList<LogEntry>());
 		}
 
 		if (toRecover) {
